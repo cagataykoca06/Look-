a06d6bdd1e5eb49cde7e1202371473b067e7a0b7
cagatay <142677193+cagataykoca06@users.noreply.github.com>
2025-11-13 13:30:09 +0300

    Firestore+Extensions.swift

--- Firestore+Extensions.swift
+++ Firestore+Extensions.swift
@@ -5,8 +5,6 @@ import FirebaseFirestoreSwift
 extension DocumentReference {
     func setData<T: Encodable>(from value: T) async throws {
         return try await withCheckedThrowingContinuation { continuation in
-            // Method only throws if there’s an encoding error, which indicates a problem with our model.
-            // We handled this with a force try, while all other errors are passed to the completion handler.
             try! setData(from: value) { error in
                 if let error = error {
                     continuation.resume(throwing: error)
@@ -26,3 +24,4 @@ extension Query {
         }
     }
 }
+
